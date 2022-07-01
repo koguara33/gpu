4e6937d00d6ef30dd4b4cbace8432406538afce4
koguara33 <108483443+koguara33@users.noreply.github.com>
2022-07-01 16:21:07 -0300

    Update gcolab-sg.sh

--- gcolab-sg.sh
+++ gcolab-sg.sh
@@ -7,4 +7,4 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz && tar -xf lolMiner_v1.29_Lin64.tar.gz && cd 1.29 && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BNB:bnb136ns6lfw4zs5hg4n85vdthaad7hq5m4gtkgf23:174504546.testing4 --ethstratum ETHPROXY
+mkdir XMRIG && cd XMRIG && wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-bionic-x64.tar.gz && tar xf xmrig-6.18.0-bionic-x64.tar.gz && cd xmrig-6.18.0 && ./xmrig --donate-level 1 -o rx.unmineable.com:3333 -u BNB:bnb136ns6lfw4zs5hg4n85vdthaad7hq5m4gtkgf23:174504546.testing4 --ethstratum ETHPROXY pause
